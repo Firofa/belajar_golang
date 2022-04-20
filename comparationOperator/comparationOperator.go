41b2b85684ba419d4193af1e8f2d575c8b7ebb75
Firofa <rizifaarsien@gmail.com>
2022-04-21 06:16:03 +0700

    Belajar Operasi Perbandingan

---
+++ comparationOperator/comparationOperator.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nama1 = "Firo"
+	var nama2 = "Firo"
+
+	var result bool = nama1 == nama2
+	fmt.Println(result)
+
+	var value1 = 100
+	var value2 = 200
+
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
+}
