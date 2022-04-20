a0e866f4005cf1f2bfc7355d51c40d9e67cd5e4c
Firofa <rizifaarsien@gmail.com>
2022-04-20 15:06:06 +0700

    Belajar Boolean

---
+++ boolean/boolean.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// Boolean
+// Kata Kunci: Bool
+// Nilai: bisa "true" & bisa "false"
+
+func main() {
+	fmt.Println("Benar = ", true)
+	fmt.Println("Salah = ", false)
+}
